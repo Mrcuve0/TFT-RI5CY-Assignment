a5d879cca7f3cc67ef2f15c2e8412286366ab591
CERINI SAMUELE YVES <s256813@po.polito.it>
2020-01-08 19:51:13 +0100

    New Parser Transition Faults + Constraints

--- lab4/stil_parser.py
+++ lab4/stil_parser.py
@@ -34,10 +34,10 @@ signals_bits = [
 	('mult_is_clpx_i', 1),
 	('mult_clpx_shift_i', 2), 
 	('mult_clpx_img_i', 1), 
-	('mult_multicycle_o', 1),
+	# ('mult_multicycle_o', 1),
 	('fpu_prec_i', 5),
-	('fpu_fflags_o', 5),
-	('fpu_fflags_we_o', 1),
+	# ('fpu_fflags_o', 5),
+	# ('fpu_fflags_we_o', 1),
 	('apu_en_i', 1),
 	('apu_op_i', 6), 
 	('apu_lat_i', 2),
@@ -46,20 +46,20 @@ signals_bits = [
 	('apu_flags_i', 15), 
 	('apu_read_regs_i', 18),
 	('apu_read_regs_valid_i', 3), 
-	('apu_read_dep_o', 1), 
+	# ('apu_read_dep_o', 1), 
 	('apu_write_regs_i', 12),
 	('apu_write_regs_valid_i', 2), 
-	('apu_write_dep_o', 1), 
-	('apu_perf_type_o', 1),
-	('apu_perf_cont_o', 1), 
-	('apu_perf_wb_o', 1), 
-	('apu_busy_o', 1), 
-	('apu_ready_wb_o', 1),
-	('apu_master_req_o', 1),
-	('apu_master_ready_o', 1), 
+	# ('apu_write_dep_o', 1), 
+	# ('apu_perf_type_o', 1),
+	# ('apu_perf_cont_o', 1), 
+	# ('apu_perf_wb_o', 1), 
+	# ('apu_busy_o', 1), 
+	# ('apu_ready_wb_o', 1),
+	# ('apu_master_req_o', 1),
+	# ('apu_master_ready_o', 1), 
 	('apu_master_gnt_i', 1),
-	('apu_master_operands_o', 96), 
-	('apu_master_op_o', 6),
+	# ('apu_master_operands_o', 96), 
+	# ('apu_master_op_o', 6),
 	('apu_master_valid_i', 1),
 	('apu_master_result_i', 32), 
 	('lsu_en_i', 1), 
@@ -70,19 +70,19 @@ signals_bits = [
 	('regfile_waddr_i', 6), 
 	('csr_access_i', 1),
 	('csr_rdata_i', 32),
-	('regfile_waddr_wb_o', 6), 
-	('regfile_we_wb_o', 1), 
-	('regfile_wdata_wb_o', 32),
-	('regfile_alu_waddr_fw_o', 6), 
-	('regfile_alu_we_fw_o', 1), 
-	('regfile_alu_wdata_fw_o', 32),
-	('jump_target_o', 32), 
-	('branch_decision_o', 1), 
+	# ('regfile_waddr_wb_o', 6), 
+	# ('regfile_we_wb_o', 1), 
+	# ('regfile_wdata_wb_o', 32),
+	# ('regfile_alu_waddr_fw_o', 6), 
+	# ('regfile_alu_we_fw_o', 1), 
+	# ('regfile_alu_wdata_fw_o', 32),
+	# ('jump_target_o', 32), 
+	# ('branch_decision_o', 1), 
 	('lsu_ready_ex_i', 1), 
-	('ex_ready_o', 1),
-	('ex_valid_o', 1), 
+	# ('ex_ready_o', 1),
+	# ('ex_valid_o', 1), 
 	('wb_ready_i', 1), 
-	('lsu_err_i_BAR', 1), 
+	('lsu_err_i_BAR', 1), #1
 	('regfile_alu_we_i', 1)]
 
 # Opcodes are listed in rtl/include/riscv_defines.sv
@@ -142,6 +142,7 @@ instr2operands = {
 }
 
 with open(stil_filename) as stil_file:
+	index = 0
 	for line in stil_file:
 
 		# Search for "_pi" line
@@ -153,39 +154,38 @@ with open(stil_filename) as stil_file:
 		# Translate "_pi" line to a dictionary
 		s = io.StringIO(pi_string)
 		pi = {name:s.read(bits) for (name,bits) in signals_bits}
-		#print(pi)
+		# print(pi)
 
-		if pi['alu_operator_i'] in instr2operands.keys():
+		if (pi['alu_operator_i'] in instr2operands.keys() and index%2 == 0):
 			instr = instr2operands[pi['alu_operator_i']]
 			rs1 = hex(int(pi['alu_operand_a_i'],2))
 			rs2 = hex(int(pi['alu_operand_b_i'],2))
-			# print('li t0, {}'.format(rs1))
-			# print('li t1, {}'.format(rs2))
-			# print('{} t2, t0, t1'.format(instr))
-			# print('sw t2, 4(sp)')
-
-			# # Swap
-			# print('{} t2, t1, t0'.format(instr))
-			# print('sw t2, 4(sp)')
-
-			# # Self
-			# print('{} t2, t0, t0'.format(instr))
-			# print('sw t2, 4(sp)')
-			# print('{} t2, t1, t1'.format(instr))
-			# print('sw t2, 4(sp)')
-			# print('')
 
 			print('li t0, {}'.format(rs1))
 			print('li t1, {}'.format(rs2))
 			print('{} t2, t0, t1'.format(instr))
-			# Swap
-			print('{} t3, t1, t0'.format(instr))
-			# Self
-			print('{} t4, t0, t0'.format(instr))
-			print('{} t5, t1, t1'.format(instr))
-			# Store
+			index = index + 1 
+		
+		elif (pi['alu_operator_i'] in instr2operands.keys() and index%2 == 1):
+			instr = instr2operands[pi['alu_operator_i']]
+			rs1 = hex(int(pi['alu_operand_a_i'],2))
+			rs2 = hex(int(pi['alu_operand_b_i'],2))
+
+			print('li t3, {}'.format(rs1))
+			print('li t4, {}'.format(rs2))
+			print('{} t5, t3, t4'.format(instr))
 			print('sw t2, 4(sp)')
-			print('sw t3, 8(sp)')
-			print('sw t4, 12(sp)')
-			print('sw t5, 16(sp)')
+			print('sw t5, 8(sp)')
 			print('')
+			index = 0
+		
+		
+		
+		# Swap
+		# print('{} t3, t1, t0'.format(instr))
+		# Self
+		# print('{} t4, t0, t0'.format(instr))
+		# print('{} t5, t1, t1'.format(instr))
+		# Store
+		# print('sw t4, 12(sp)')
+		# print('sw t5, 16(sp)')
